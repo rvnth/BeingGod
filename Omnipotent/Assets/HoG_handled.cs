0000fe506231da24db4a8a1c4b95b0390fa72636
sophont01 <saurabh0saini@gmail.com>
2017-11-06 00:50:54 +0530

    Added handler for Hand of God

---
+++ Omnipotent/Assets/HoG_handled.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoG_handled : MonoBehaviour {
+
+	public GameObject EoG;
+
+	public bool[] faceCreated;
+	private bool allFaceCreated = false;
+
+	// Use this for initialization
+	void Start () {
+		faceCreated = new bool[] {false,false,false,false};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (allFaceCreated) {
+			//EoG.GetComponent<TrackingAction> ().enabled = true;
+			faceCreated [0] = false;
+			allFaceCreated = false;
+		}
+		else if (faceCreated [0] && faceCreated [1] && faceCreated [2] && faceCreated [3])
+			allFaceCreated = true;
+	}
+}
